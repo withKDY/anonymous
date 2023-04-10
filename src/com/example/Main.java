60703b13925b18109360ac3e263804363bd7c4f8
김대용 <77915737+withKDY@users.noreply.github.com>
2023-04-10 16:50:54 +0900

    리스트 옮기는 예제

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,32 +1,47 @@
 package com.example;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            int x, y;
-            Scanner in = new Scanner(System.in);
+//            int x, y;
+//            Scanner in = new Scanner(System.in);
+//
+//            System.out.print("x를 입력하세요 : ");
+//            x = in.nextInt();
+//
+//            System.out.print("y를 입력하세요 : ");
+//            y = in.nextInt();
+//
+//            MyFunction mf1 = (a, b) -> a + b;
+//            MyFunction mf2 = (a, b) -> a - b;
+//            MyFunction mf3 = (a, b) -> a * b;
+//            MyFunction mf4 = (a, b) -> a / b;
+//
+//            System.out.println(mf1.calc(x, y));
+//            System.out.println(mf2.calc(x, y));
+//            System.out.println(mf3.calc(x, y));
+//            System.out.println(mf4.calc(x, y));
 
-            System.out.print("x를 입력하세요 : ");
-            x = in.nextInt();
+            Set<Integer> set = new HashSet<>();
+            Random random = new Random();
+            List list =  new ArrayList<>();
 
-            System.out.print("y를 입력하세요 : ");
-            y = in.nextInt();
+            int i = 0;
 
-            MyFunction mf1 = (a, b) -> a + b;
-            MyFunction mf2 = (a, b) -> a - b;
-            MyFunction mf3 = (a, b) -> a * b;
-            MyFunction mf4 = (a, b) -> a / b;
+            while (i < 11) {
+                set.add(random.nextInt(100));
+                i++;
+            }
 
-            System.out.println(mf1.calc(x, y));
+            set.forEach(list::add);
+
+            Collections.sort(list);
+            System.out.println(list);
+
             Thread.sleep(1000);
-            System.out.println(mf2.calc(x, y));
-            Thread.sleep(1000);
-            System.out.println(mf3.calc(x, y));
-            Thread.sleep(1000);
-            System.out.println(mf4.calc(x, y));
 
         }catch (InterruptedException e) {
             e.printStackTrace();
@@ -35,5 +50,6 @@ public class Main {
 
     private interface MyFunction { // 함수형 인터페이스
         int calc(int x, int y); // 추상 메소드
+
     }
 }
